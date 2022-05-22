dd705715612b6d82873f3dfae1589e509762fe89
baku <baku@baku.world>
2022-05-22 15:59:19 +0000

    Update extension.ts

--- src/extension.ts
+++ src/extension.ts
@@ -4,16 +4,7 @@ import * as utils from "./modules/utils";
 export function activate(context: vscode.ExtensionContext) {
   // console.log("Activate.");
 
-  let start = vscode.commands.registerCommand("nikanikaberry.start", () => {
-    utils.startTimer(60000);
-  });
-  let stop = vscode.commands.registerCommand("nikanikaberry.stop", () => {
-    utils.stopTimer();
-    vscode.window.showInformationMessage("ストップしました");
-  });
-
-  context.subscriptions.push(start);
-  context.subscriptions.push(stop);
+  utils.startTimer(60000);
 }
 
 export function deactivate() {
